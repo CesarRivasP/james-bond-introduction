71f3c7fd7faab1423163d81ffd619155f2da6455
CesarRivasP <cesarjose58@gmail.com>
2019-05-10 17:19:37 -0400

    Implemented items of circle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const circleCount = Math.ceil(window.innerWidth / 200);
 const circles = Array.from(Array(circleCount).keys());
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        {
-          circles.map((i) => {
-            return <div className="circle"/>
-          })
-        }
+
+class App extends Component {
+  state = {
+    animate: false
+  }
+
+  componentDidMount(){
+      setTimeout(() => {
+        this.setState({
+          animate: true
+        })
+      }, 0);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <div
+          className="circle mover"
+          style={{
+            transform: `translateX(${!this.state.animate ? "0px" : circleCount * 200}px) translateY(-50%)`,
+            transition: `transform ${circleCount * 0.75}s linear`
+          }}
+        />
+        <div className="container">
+          {
+            circles.map((i) => {
+              return (
+                <div
+                  className="circle item"
+                  key={i}
+                  style={{ animationDelay: `${i * 0.75}s`}}
+                />
+              )
+            })
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
