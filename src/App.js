b917a2fb392b472feba558b34a779577f96a2a72
CesarRivasP <cesarjose58@gmail.com>
2019-05-10 14:28:00 -0400

    Implemented circles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const circleCount = Math.ceil(window.innerWidth / 200);
+const circles = Array.from(Array(circleCount).keys());
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        {
+          circles.map((i) => {
+            return <div className="circle"/>
+          })
+        }
+      </div>
     </div>
   );
 }
